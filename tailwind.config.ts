ea657d0d3c81a5a3cbe56c73b57e17de3ab0e54b
Samuel <samuelciga2004@gmail.com>
2025-05-24 18:40:51 +0200

    Iniciada la LandPage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui"
 
 export default {
   content: [
@@ -8,11 +9,30 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      fontFamily: {
+        cozy_text: ["'Quicksand'", "'cursive'"],
+        cozy_title: ["'Pacifico'", "'cursive'"]
+      }
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        cozytheme: {
+          "primary": "#A97155",
+          "secondary": "#FAD6A5",
+          "accent": "#C9F2C7",
+          "neutral": "#f2efe5",
+          "base-100": "#fffdf7",
+          "info": "#C4DBF6",
+          "success": "#A3E4D7",
+          "warning": "#F9E79F",
+          "error": "#F1948A",
+        }
+      }
+    ],
+    darkTheme: "cozytheme",
+    base: true,
+  }
 } satisfies Config;
